db2ab7bfba8dc01244160a6cc9a452c34792c6a2
Maksim Veprev <maxveprevspb@gmail.com>
2022-10-21 17:03:58 +0100

    Add an if clause to raise a ValueError if 0 or negative number has been passed to the method primes

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+
+def isPrime(n):
+    for i in range(2, n):
+        if(n%i==0):
+            return False
+    return True
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError()
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        if(isPrime(i)):
+            list.append(i)
+        i += 1
     return list
